7096b4f21653fa46d9582db492687233db86a667
RyanThomas_Harrington <rtharrin@outlook.com>
2025-03-18 20:01:17 -0400

    Initial commit in class

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='Simple Demo package',
+      url='https://github.com/rtharrin/DS5100-Lesson09',
+      author='Ryan Harrington',
+      author_email='crf6zj@virginia.edu',
+      license='MIT',
+      packages=['demo'])
